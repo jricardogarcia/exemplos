00732800cf6bd9b7e2f2be6a25a7fc5da6404d33
Jose Ricardo Garcia <jgarcia@gvdasa.com.br>
2021-11-18 21:44:18 -0300

    Adicionado métodos para manipulação dos filmes.

---
+++ GvFlix/Dal/Models/Filme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dal.Models
+{
+    public class Filme
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        public DateTime DataLancamento { get; set; }
+
+        [Required]
+        public DateTime Duracao { get; set; }
+
+        public string Resumo { get; set; }
+    }
+}
